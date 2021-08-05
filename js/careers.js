643e8705b1b622de15e4c6bcdb6565ab39d5072a
dsj121 <87056157+dsj121@users.noreply.github.com>
2021-08-05 17:53:20 +0900

    v1.01
    careers.html + animation

--- js/careers.js
+++ js/careers.js
@@ -1,3 +1,14 @@
+// on load event
+let windowWidth = $(window).width();
+
+function carOnload() {
+  $(".banner img").animate({ top: "0px" }, 1500);
+  $(".title h1").animate({ opacity: 1 }, 500, function () {
+    $(".title p").animate({ opacity: 1 }, 500);
+  });
+}
+carOnload();
+
 function textChange(index) {
   $(".textBox").removeClass("active");
   $(".textBox").eq(index).addClass("active");
